c7515c3ab78c8e685f1047657e10e0b8f218e768
Camila Goncalves <camiladenise.96@gmail.com>
2025-05-08 10:39:26 -0300

    validation with ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { Request } from './modules/statistics/core/interfaces/request.interface';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StatisticsService } from './modules/statistics/core/services/statistics.service';
 import { StatisticsComponent } from './modules/statistics/components/statistics/statistics.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, StatisticsComponent],
   standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule, StatisticsComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -25,11 +26,21 @@ export class AppComponent {
     });
   }
 
+  onClear(): void {
+    this.form.reset();
+  }
+
   onSubmit() {
     if (this.form.valid) {
+
+      if (this.form.value.upperLimit < this.form.value.lowerLimit) {
+        this.form.setErrors({ upperLessThanLower: true });
+        return;
+      }
+
       const requestData: Request = {
-        lowerLimit: +this.form.value.lowerLimit,
-        upperLimit: +this.form.value.upperLimit,
+        lowerLimit: this.form.value.lowerLimit,
+        upperLimit: this.form.value.upperLimit,
         values: this.form.value.values.split(',').map(Number)
       };
 
@@ -42,5 +53,18 @@ export class AppComponent {
         }
       });
     }
+  }
+
+  upperGreaterThanLower(): ValidatorFn {
+    return (group: AbstractControl): ValidationErrors | null => {
+      const lower = Number(group.get('lowerLimit')?.value);
+      const upper = Number(group.get('upperLimit')?.value);
+
+      if (lower !== null && upper !== null && upper < lower) {
+        return { upperLessThanLower: true };
+      }
+
+      return null;
+    };
   }
 }
